fc9fdeae852b61fcb1cbc337f3bcea66bcadeb4f
bek06371 <bek06371@LC201700003.bekoag.local>
2019-12-18 15:25:48 +0100

    updates version

--- janitor/janitor.lib.js
+++ janitor/janitor.lib.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        JANITOR – Java API Navigation Is The Optimal Rescue (lib)
 // @description Inserts a navigation tree for modules, packages and types (interfaces, classes, enums, exceptions, errors, annotations) into the Javadoc pages of Java 11+.
-// @version     19.12.17
+// @version     19.12.18
 // @author      Gerold 'Geri' Broser <https://stackoverflow.com/users/1744774>
 // @icon        https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Faenza-openjdk-6.svg/96px-Faenza-openjdk-6.svg.png
 // @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
