f5cfb3fc4b729e4fb3fe465097b39d579c8be5f6
bek06371 <bek06371@LC201700003.bekoag.local>
2019-12-19 12:57:21 +0100

    adds set-version test files

---
+++ gitattributes.test/version-test.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         version-test.user.js
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @grant        none
+// ==/UserScript==
+
+( function() {
+	'use strict';
+
+	// Your code here...
+} )();
