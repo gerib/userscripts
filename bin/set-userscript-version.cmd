3a858c93985b5338c8d2dcee23cd517a0717f1d4
Gerold 'Geri' Broser <gerib@users.noreply.github.com>
2019-12-22 23:38:52 +0100

    updates s-u-v

--- bin/set-userscript-version.cmd
+++ bin/set-userscript-version.cmd
@@ -1,8 +1,9 @@
 @echo off
+@setlocal EnableDelayedExpansion
+@set prompt=$g$s 
+@set out=bin\suv.out
 
-setlocal EnableDelayedExpansion
-
-echo "%1">>s-u-v.log
+@echo:>%out%
 
 :: See: Read stdin stream in a batch file <https://stackoverflow.com/a/6980605/1744774>
 for /f "tokens=*" %%l in ( 'findstr /n $' ) do (
@@ -11,12 +12,14 @@ for /f "tokens=*" %%l in ( 'findstr /n $' ) do (
     set time_=0%time: =%
 	if "!version!"=="// @version " (
 	  echo // ^@version     %date:~8,2%.%date:~3,2%.%date:~0,2%-!time_:~-11,2!!time_:~-8,2!!time_:~-5,2!
+	  echo // ^@version     %date:~8,2%.%date:~3,2%.%date:~0,2%-!time_:~-11,2!!time_:~-8,2!!time_:~-5,2!>>%out%
 	) else (
 	  set line=!line:*:=!
-	  echo:!line!
+	  echo:!line! && echo:!line!>>%out%
 	)
 )
 
-git checkout-index
+:: Doesn't work since it is executed before adding
+::git diff --cached --name-only>>%out%
 
-endlocal
+@endlocal
--- gitattributes.test/set-version-test.lib.js
+++ gitattributes.test/set-version-test.lib.js
@@ -1,15 +1,15 @@
-// ==UserScript==
-// @name         version-test.lib.js
-// @namespace    http://tampermonkey.net/
-// @version     19.12.22-034935
-// @description  try to take over the world
-// @author       You
-// @match        http://*/*
-// @grant        none
-// ==/UserScript==
-
-( function() {
-	'use strict';
-
-	// Your code here...
-} )();
+// ==UserScript== 
+// @name         version-test.lib.js 
+// @namespace    http://tampermonkey.net/ 
+// @version     19.12.22-233646
+// @description  try to take over the world 
+// @author       You 
+// @match        http://*/* 
+// @grant        none 
+// ==/UserScript== 
+ 
+( function() { 
+	'use strict'; 
+ 
+	// Your code here... 
+} )(); 
