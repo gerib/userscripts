6897f78a30dbe02ca88fcb9f382668f10015e998
Geri <Geri@XMG>
2019-12-23 19:56:25 +0100

    updates href

--- janitor/janitor.lib.js
+++ janitor/janitor.lib.js
@@ -1,7 +1,7 @@
 // ==UserScript==  
 // @name        JANITOR – Java API Navigation Is The Only Rescue (lib)  
 // @description Inserts a navigation tree for modules, packages and types (interfaces, classes, enums, exceptions, errors, annotations) into the Javadoc pages of Java 11+.  
-// @version     19.12.23-190112
+// @version     19.12.23-195612
 // @author      Gerold 'Geri' Broser <https://stackoverflow.com/users/1744774>  
 // @icon        https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Faenza-openjdk-6.svg/96px-Faenza-openjdk-6.svg.png  
 // @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>  
@@ -196,7 +196,8 @@ function addModulesOrPackages( ofType, fromURL, toParent, parentName) {
 			// Link for packages: https://docs.oracle.com/en/java/javase/1{n}/docs/api/{module.name}/{package/path}/package-summary.html 
 			// TODO: Doesn't work in Chrome (79.0.3945.88) even if this line is commented out. See NOTE above. 
 			if ( a.href.startsWith("http") ) 
-				a.href = `${API_URL}/`+ a.href.substr( a.href.indexOf( a.innerText ) ) 
+				// See »How to get the nth occurrence in a string?« <https://stackoverflow.com/a/14480366/1744774> 
+				a.href = `${API_URL}/`+ a.href.substr( a.href.split( a.href, 1).join( a.href ).length ) 
 			else 
 				a.href = `${API_URL}/${parentName}/${a.href}` 
 			const aTitle = `${ofType} ${a.innerText}` 
