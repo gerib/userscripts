1d58a6cc749fa97cdd8e15c0ee2333cae4953655
Geri <Geri@XMG>
2019-12-23 17:54:47 +0100

    updates versioning

--- gitattributes.test/set-version-test.lib.js
+++ gitattributes.test/set-version-test.lib.js
@@ -1,15 +1,15 @@
-// ==UserScript== 
-// @name         version-test.lib.js 
-// @namespace    http://tampermonkey.net/ 
-// @version     19.12.22-233646
-// @description  try to take over the world 
-// @author       You 
-// @match        http://*/* 
-// @grant        none 
-// ==/UserScript== 
- 
-( function() { 
-	'use strict'; 
- 
-	// Your code here... 
-} )(); 
+// ==UserScript==  
+// @name         version-test.lib.js  
+// @namespace    http://tampermonkey.net/  
+// @version     19.12.23-175417
+// @description  try to take over the world  
+// @author       You  
+// @match        http://*/*  
+// @grant        none  
+// ==/UserScript==  
+  
+( function() {  
+	'use strict';  
+  
+	// Your code here...  
+} )();  
--- gitattributes.test/set-version-test.user.js
+++ gitattributes.test/set-version-test.user.js
@@ -1,16 +1,16 @@
-// ==UserScript== 
-// @name         version-test.user.js 
-// @namespace    http://tampermonkey.net/ 
-// @version     19.12.22-233656
-// @description  try to take over the world 
-// @author       You 
-// @match        http://*/* 
-// @grant        none 
-// ==/UserScript== 
- 
-( function() { 
-	'use strict'; 
- 
-	// Your code here... 
-	 
-} )(); 
+// ==UserScript==  
+// @name         version-test.user.js  
+// @namespace    http://tampermonkey.net/  
+// @version     19.12.23-175417
+// @description  try to take over the world  
+// @author       You  
+// @match        http://*/*  
+// @grant        none  
+// ==/UserScript==  
+  
+( function() {  
+	'use strict';  
+  
+	// Your code here...  
+	  
+} )();  
