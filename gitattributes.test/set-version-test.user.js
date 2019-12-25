5a399a418849167c946356a925813d99b514ccda
Gerold 'Geri' Broser <gerib@users.noreply.github.com>
2019-12-25 01:01:40 +0100

    ?

--- gitattributes.test/set-version-test.user.js
+++ gitattributes.test/set-version-test.user.js
@@ -1,16 +1,16 @@
-// ==UserScript==   
-// @name         version-test.user.js   
-// @namespace    http://tampermonkey.net/   
-// @version     .20.12-211402
-// @description  try to take over the world   
-// @author       You   
-// @match        http://*/*   
-// @grant        none   
-// ==/UserScript==   
-   
-( function() {   
-	'use strict';   
-   
-	// Your code here...   
-	   
-} )();   
+// ==UserScript==  
+// @name         version-test.user.js  
+// @namespace    http://tampermonkey.net/  
+// @version     19.12.23-175417
+// @description  try to take over the world  
+// @author       You  
+// @match        http://*/*  
+// @grant        none  
+// ==/UserScript==  
+  
+( function() {  
+	'use strict';  
+  
+	// Your code here...  
+	  
+} )();  
