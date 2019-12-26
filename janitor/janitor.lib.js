2f07ecc1f97981c4f83fc26a99be6cda8182132e
Gerold 'Geri' Broser <gerib@users.noreply.github.com>
2019-12-26 23:16:08 +0100

    corrects a.title

--- janitor/janitor.lib.js
+++ janitor/janitor.lib.js
@@ -1,7 +1,7 @@
 // ==UserScript== 
 // @name        JANITOR – Java API Navigation Is The Only Rescue (lib) 
 // @description Inserts a navigation tree for modules, packages and types (interfaces, classes, enums, exceptions, errors, annotations) into the Javadoc pages of Java 11+. 
-// @version     19.12.26-230915
+// @version     19.12.26-231557
 // @author      Gerold 'Geri' Broser <https://stackoverflow.com/users/1744774> 
 // @icon        https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Faenza-openjdk-6.svg/96px-Faenza-openjdk-6.svg.png 
 // @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html> 
@@ -74,7 +74,7 @@ const ICONS = new Map( TYPE_LETTERS_IN_CIRCLE
 		: [['Module',"M"],['Package',"P"],['Interface',"I"],['Class',"C"],['Enum',"E<sub>n</sub>"], 
 		   ['Exception',"E<sub>x</sub>"],['Error',"E<sub>r</sub>"],['Annotation',"A"]] ) 
  
-//JANITOR() // for developing 
+JANITOR() // for developing 
  
 function JANITOR() { 
  
@@ -180,7 +180,7 @@ function addModulesOrPackages( ofType, fromURL, toParent, parentName) {
 			const a = link 
 			a.href = `${API_URL}/${parentName}/${a.getAttribute('href')}` 
 			a.title = `${ofType} ${a.innerText}` 
-			summary.innerHTML = `<span title="${aTitle}" style="cursor: default;">${branch} &nbsp;</span>` 
+			summary.innerHTML = `<span title="${a.title}" style="cursor: default;">${branch} &nbsp;</span>` 
  
 			summary.addEventListener( 'click', function() { 
 				ofType === 'Module' 
@@ -259,7 +259,7 @@ function addTypes( ofType, fromURL, toParent, moduleName, packageName, typeCount
 				const highlight = document.URL.includes( `/${a.innerText}.html` ) 
 				const icon = `<span style='color:${COLORS.get( ofType )};${highlight ? 'font-weight:bold': ''}'>${ICONS.get( ofType )}</span>` 
 				const branch = `&nbsp; &nbsp; ･&nbsp; &nbsp;&thinsp;${--typeCount > 0 ? "├" : "└"}─ ${icon}` 
-				summary.innerHTML = `<span title='${aTitle}' style='cursor:default;'>${branch} &nbsp;</span>` 
+				summary.innerHTML = `<span title='${a.title}' style='cursor:default;'>${branch} &nbsp;</span>` 
  
 				summary.appendChild( a ) 
 				details.appendChild( summary ) 
