030b10853eff82f34b6c80df3732e28f71b48597
Gerold 'Geri' Broser <gerib@users.noreply.github.com>
2019-12-26 18:04:56 +0100

    updates doc

--- janitor/janitor.lib.js
+++ janitor/janitor.lib.js
@@ -1,7 +1,7 @@
 // ==UserScript== 
 // @name        JANITOR – Java API Navigation Is The Only Rescue (lib) 
 // @description Inserts a navigation tree for modules, packages and types (interfaces, classes, enums, exceptions, errors, annotations) into the Javadoc pages of Java 11+. 
-// @version     19.12.26-180324
+// @version     19.12.26-180447
 // @author      Gerold 'Geri' Broser <https://stackoverflow.com/users/1744774> 
 // @icon        https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Faenza-openjdk-6.svg/96px-Faenza-openjdk-6.svg.png 
 // @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html> 
@@ -36,7 +36,7 @@
  *     | |   <summary>*¹ | <span>*² 
  *     | |     <span>{branch} 
  *     | |       <span>{icon} 
- *     | | <a href='{module, package or type page}'>{module, package or type name}</a> 
+ *     | |         <a href='{module, package or type page}'>{module, package or type name}</a> 
  *     | <header> 
  *     | <main> 
  *     <footer> 
