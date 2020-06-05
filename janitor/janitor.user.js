16ac9ba50854dde60b942da41245599a232b1f81
Gerold 'Geri' Broser <5280567-gerib@users.noreply.gitlab.com>
2020-06-05 18:24:20 +0000

    adapts URLs

--- janitor/janitor.user.js
+++ janitor/janitor.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name        JANITOR – Java API Navigation Is The Only Rescue
 // @description Inserts a navigation tree for modules, packages and types (interfaces, classes, enums, exceptions, errors, annotations) into the Javadoc pages of Java 11+.
-// @version     20.05.06-2020
+// @version     20.05.06-2025
 // @author      Gerold 'Geri' Broser <https://stackoverflow.com/users/1744774>
 // @icon        https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Faenza-openjdk-6.svg/96px-Faenza-openjdk-6.svg.png
 // @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
 // @homepage    https://gitlab.com/gerib/userscripts/-/wikis/JANITOR-%E2%80%93-Java-API-Navigation-Is-The-Only-Rescue
 // @supportURL  https://gitlab.com/gerib/userscripts/-/issues
-// @downloadURL https://gitlab.com/gerib/userscripts/-/raw/master/janitor/janitor.lib.js
-// @updateURL   https://gitlab.com/gerib/userscripts/-/raw/master/janitor/janitor.lib.js
+// @downloadURL https://gitlab.com/gerib/userscripts/-/raw/master/janitor/janitor.user.js
+// @updateURL   https://gitlab.com/gerib/userscripts/-/raw/master/janitor/janitor.user.js
 // ---------------------------------------------------------
 // @namespace   igb
 // @require     https://gitlab.com/gerib/userscripts/-/raw/master/janitor/janitor.lib.js
